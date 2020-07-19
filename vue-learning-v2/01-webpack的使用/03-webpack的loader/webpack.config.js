0dbb1c77e7e28b1def011d5f21ece71dc69a6526
cqboyone <cqboyone@163.com>
2020-07-20 00:02:05 +0800

    [add]增加图片打包

--- vue-learning-v2/01-webpack的使用/03-webpack的loader/webpack.config.js
+++ vue-learning-v2/01-webpack的使用/03-webpack的loader/webpack.config.js
@@ -3,8 +3,9 @@ module.exports = {
   entry: './src/main.js',
   output: {
     //动态获取文件路径后再拼接
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: 'dist/'
   },
   module: {
     rules: [
@@ -13,7 +14,7 @@ module.exports = {
         // css-loader只负责加载css文件
         // style-loader负责将样式添加到dom中
         // 使用多个loader时，从右向左，所以css-loader放右边
-        use: ['style-loader','css-loader']
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.less$/,
@@ -24,6 +25,20 @@ module.exports = {
         }, {
           loader: "less-loader" // compiles Less to CSS
         }]
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // 当加载的图片小于limit时，会将图片编译成base46
+              // 当图片大于limit时，需要使用file-loader
+              limit: 8192,
+              name: 'img/[name].[hash:8].[ext]'
+            }
+          }
+        ]
       }
     ]
   }
