87b7f42c76d953a1195c780854fbd20d896257ce
cqboyone <cqboyone@163.com>
2020-08-09 19:30:57 +0800

    补充

--- vue-learning-v2/07-vuerouter/src/components/User.vue
+++ vue-learning-v2/07-vuerouter/src/components/User.vue
@@ -2,17 +2,26 @@
   <div>
     <h2>我是用户页面</h2>
     <p>哈哈哈哈哈</p>
-    <h2>{{userId}}</h2>
+    <h2>{{ userId }}</h2>
+    <button @click="btnClick">按钮</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "User",
-  computed:{
-    userId(){
+  computed: {
+    userId() {
       //这里的$route指的是routers的数组里面当前活跃的路由对象
       return this.$route.params.userId2
+    }
+  },
+  methods: {
+    btnClick() {
+      console.log(this.$router);
+      console.log(this.$route);
+      // 所有的组件都继承自Vue的原型,这里没有定义name，但是在main.js定义了，所以可以获取
+      console.log(this.name)
     }
   }
 }
--- vue-learning-v2/07-vuerouter/src/main.js
+++ vue-learning-v2/07-vuerouter/src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.prototype.name= 'vv'
 
 /* eslint-disable no-new */
 new Vue({
